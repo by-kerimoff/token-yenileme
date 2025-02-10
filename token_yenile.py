912c3f63c98d25bf9e63e4443388ac38d51b4ece
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-10 04:30:22 +0400

    Update token_yenile.py

--- token_yenile.py
+++ token_yenile.py
@@ -5,14 +5,14 @@ import requests
 def get_new_token():
     # Burada yeni token alınır. Məsələn, bir API istifadə edə bilərsiniz.
     # API ünvanını və parametrləri əlavə edin.
-    api_url = "https://www.ecanlitvizle.app//getToken"  # Token alınan API ünvanı
+    api_url = "https://www.canlitv.me//getToken"  # Token alınan API ünvanı
     response = requests.get(api_url)
     new_token = response.json()["token"]  # API cavabından tokeni çıxarın
     return new_token
 
 # Linki yeniləyən funksiya
 def update_m3u8_link(token):
-    base_url = "https://ecanlitv3.etvserver.com/xazartv.m3u8"
+    base_url = "https://cdn7.canlitv.me/kanald.m3u8"
     tms = int(time.time())  # Hazırki zamanı alırıq
     new_link = f"{base_url}?tkn={token}&tms={tms}"
     return new_link
